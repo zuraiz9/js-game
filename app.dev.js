1ebafe632801ec49681952f500f38ac1377fafcf
Zuraiz Choudhary <zuraiz@Zuraizs-MacBook-Air.local>
2021-07-02 10:49:21 +0100

    score updates on click

--- app.dev.js
+++ app.dev.js
@@ -3,7 +3,9 @@
 var squares = document.querySelectorAll(".square");
 var timeLeft = document.querySelector("#timeleft");
 var score = document.querySelector("#score");
-var moveTimePeriod = null; // creating the first random moving circle on grid that moves after a set time
+var currentScore = 0;
+var moveTimePeriod = null;
+var clickPosition; // creating the first random moving circle on grid that moves after a set time
 
 var randomPlace = function randomPlace() {
   squares.forEach(function (square) {
@@ -12,13 +14,23 @@ var randomPlace = function randomPlace() {
   var randomPlace = squares[Math.floor(Math.random() * 16)]; // console.log(randomPlace);
 
   randomPlace.classList.add("grn__circle");
-};
+  clickPosition = randomPlace.id; //
+}; // gets auto moving..
+
 
 var startMove = function startMove() {
   moveTimePeriod = setInterval(randomPlace, 1000);
 };
 
 startMove(); // when click on correct circle.. score gets updated
-// when click on wrong object.. game ends
+
+squares.forEach(function (square) {
+  square.addEventListener("click", function () {
+    if (square.id == clickPosition) {
+      currentScore += 1;
+      score.innerHTML = currentScore;
+    }
+  });
+}); // when click on wrong object.. game ends
 // timer counts down to 0
 // score +=1
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const squares = document.querySelectorAll(".square")
 const timeLeft = document.querySelector("#timeleft")
 const score = document.querySelector("#score")
 
+let currentScore = 0;
 let moveTimePeriod = null;
+let clickPosition;
 
 // creating the first random moving circle on grid that moves after a set time
 const randomPlace = () => {
@@ -12,13 +14,23 @@ const randomPlace = () => {
     let randomPlace = squares[Math.floor(Math.random() * 16)]
     // console.log(randomPlace);
     randomPlace.classList.add("grn__circle")
+    
+    clickPosition = randomPlace.id //
 }
-
+// gets auto moving..
 const startMove = () => {
     moveTimePeriod = setInterval(randomPlace, 1000)
 }
 startMove()
 // when click on correct circle.. score gets updated
+squares.forEach(square => {
+    square.addEventListener("click", () => {
+        if (square.id == clickPosition) {
+            currentScore += 1;
+            score.innerHTML = currentScore
+        }
+    })
+})
 // when click on wrong object.. game ends
 // timer counts down to 0
 // score +=1
