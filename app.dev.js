fde89321f62997249f5927cc2d72a319aa8bcbf2
Zuraiz Choudhary <zuraiz@Zuraizs-MacBook-Air.local>
2021-07-02 09:46:09 +0100

    js logic

--- app.dev.js
+++ app.dev.js
@@ -2,4 +2,8 @@
 
 var squares = document.querySelectorAll(".square");
 var timeLeft = document.querySelector("#timeleft");
-var score = document.querySelector("#score");
+var score = document.querySelector("#score"); // creating the first random moving circle on grid that moves after a set time
+// when click on correct circle.. score gets updated
+// when click on wrong object.. game ends
+// timer counts down to 0
+// score +=1
--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const squares = document.querySelectorAll(".square")
 const timeLeft = document.querySelector("#timeleft")
 const score = document.querySelector("#score")
+
+// creating the first random moving circle on grid that moves after a set time
+const randomPlace = () =>
+// when click on correct circle.. score gets updated
+// when click on wrong object.. game ends
+// timer counts down to 0
+// score +=1
