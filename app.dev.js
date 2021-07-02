0f249a17c1efa9ffb498e64146bdffd7c2779aed
Zuraiz Choudhary <zuraiz@Zuraizs-MacBook-Air.local>
2021-07-02 10:09:54 +0100

    auto moving

--- app.dev.js
+++ app.dev.js
@@ -2,7 +2,8 @@
 
 var squares = document.querySelectorAll(".square");
 var timeLeft = document.querySelector("#timeleft");
-var score = document.querySelector("#score"); // creating the first random moving circle on grid that moves after a set time
+var score = document.querySelector("#score");
+var moveTimePeriod = null; // creating the first random moving circle on grid that moves after a set time
 
 var randomPlace = function randomPlace() {
   squares.forEach(function (square) {
@@ -13,7 +14,11 @@ var randomPlace = function randomPlace() {
   randomPlace.classList.add("grn__circle");
 };
 
-randomPlace(); // when click on correct circle.. score gets updated
+var startMove = function startMove() {
+  moveTimePeriod = setInterval(randomPlace, 1000);
+};
+
+startMove(); // when click on correct circle.. score gets updated
 // when click on wrong object.. game ends
 // timer counts down to 0
 // score +=1
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const squares = document.querySelectorAll(".square")
 const timeLeft = document.querySelector("#timeleft")
 const score = document.querySelector("#score")
 
+let moveTimePeriod = null;
+
 // creating the first random moving circle on grid that moves after a set time
 const randomPlace = () => {
     squares.forEach(square => {
@@ -11,7 +13,11 @@ const randomPlace = () => {
     // console.log(randomPlace);
     randomPlace.classList.add("grn__circle")
 }
-randomPlace()
+
+const startMove = () => {
+    moveTimePeriod = setInterval(randomPlace, 1000)
+}
+startMove()
 // when click on correct circle.. score gets updated
 // when click on wrong object.. game ends
 // timer counts down to 0
