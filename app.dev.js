f195df7d90b44050d8c2aba118234b8e9d128113
Zuraiz Choudhary <zuraiz@Zuraizs-MacBook-Air.local>
2021-07-02 09:39:19 +0100

    initial js variables

---
+++ app.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var squares = document.querySelectorAll(".square");
+var timeLeft = document.querySelector("#timeleft");
+var score = document.querySelector("#score");
--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-
+const squares = document.querySelectorAll(".square")
+const timeLeft = document.querySelector("#timeleft")
+const score = document.querySelector("#score")
